17f2b3e0373da099c87bde429e490e920decac08
Constantine Karnaukhov <1119@zolotoykod.ru>
2017-12-07 15:33:08 +0400

    feat(docker): update docker config

---
+++ new-project/bitrix/.settings.php
@@ -0,0 +1,62 @@
+<?php
+return [
+	'utf_mode' => [
+		'value' => true,
+		'readonly' => true,
+	],
+	'cache_flags' => [
+		'value' => [
+			'config_options' => 3600.0,
+			'site_domain' => 3600.0,
+		],
+		'readonly' => false,
+	],
+	'cache' => [
+		'value' => [
+			'type' => 'memcache',
+			'sid' => 'master',
+			'memcache' => [
+				'host' => 'memcached',
+				'port' => '11211'
+			],
+		],
+		'readonly' => true,
+	],
+	'cookies' => [
+		'value' => [
+			'secure' => false,
+			'http_only' => true,
+		],
+		'readonly' => false,
+	],
+	'exception_handling' => [
+		'value' => [
+			'debug' => true,
+			'handled_errors_types' => 4437,
+			'exception_errors_types' => 4437,
+			'ignore_silence' => false,
+			'assertion_throws_exception' => true,
+			'assertion_error_type' => 256,
+			'log' => [
+				'settings' => [
+					'file' => '/var/log/php/exceptions.log',
+					'log_size' => 1000000,
+				],
+			],
+		],
+		'readonly' => false,
+	],
+	'connections' => [
+		'value' => [
+			'default' => [
+				'className' => '\\Bitrix\\Main\\DB\\MysqliConnection',
+				'host' => 'mysql',
+				'database' => 'bitrix',
+				'login' => 'root',
+				'password' => '123',
+				'options' => 2,
+			],
+		],
+		'readonly' => true,
+	]
+];
